f76228523d611748579f98b514ac255d9349bee9
Thomas Depraz <t.deprazdepland@rubika-edu.com>
2021-11-15 21:41:20 +0100

    added base shooter project package

--- WorkshopUnityTool/Assets/Editor/CharacterAttackInspector.cs
+++ WorkshopUnityTool/Assets/Editor/CharacterAttackInspector.cs
@@ -8,36 +8,82 @@ using UnityEngine;
 public class CharacterAttackInspector : Editor
 {
     CharacterAttack script;
+    SerializedProperty selfTransform;
+    SerializedProperty bulletPrefab;
+    SerializedProperty bullets;
+    public SerializedProperty count;
+    public int _count;
+
 
     public GUIStyle titleStyle;
 
     //Private Variables
+    bool doOnce;
     GameObject bulletPoolParent;
 
     void OnEnable()
     {
         script = target as CharacterAttack;
+        selfTransform = serializedObject.FindProperty(nameof(script.self));
+        bulletPrefab = serializedObject.FindProperty(nameof(script.bulletPrefab));
+        bullets = serializedObject.FindProperty(nameof(script.bullets));
+
+        SerializedObject so = new SerializedObject(this);
+        count = so.FindProperty("_count"); 
     }
 
     public override void OnInspectorGUI()
     {
-        script.self = EditorGUILayout.ObjectField(nameof(script.self), script.self, typeof(Transform),true) as Transform;
+        #region Create GUIStyle
+        //Create Style
+        if(!doOnce)
+        {
+            titleStyle = new GUIStyle(EditorStyles.label);
+            titleStyle.fontSize = 32;
+            titleStyle.alignment = TextAnchor.MiddleCenter;
+            titleStyle.fontStyle = FontStyle.Bold;
+            doOnce = true;
+        }
+        #endregion
 
+        EditorGUI.BeginDisabledGroup(true);
+        EditorGUILayout.ObjectField("Script", script, typeof(CharacterAttack),true);
+        EditorGUI.EndDisabledGroup();
+
+        serializedObject.Update(); 
+
+        EditorGUILayout.PropertyField(selfTransform);
+        EditorGUILayout.PropertyField(bulletPrefab);
+        EditorGUILayout.PropertyField(count);
+
+        #region BulletPool
+        EditorGUILayout.LabelField("Bullet Pool", titleStyle, GUILayout.MinHeight(40));
         EditorGUILayout.BeginHorizontal();//Buttons for bullet list control
 
         bool add = GUILayout.Button("+");
         bool remove = GUILayout.Button("-");
-        int count = 0; 
-        count = EditorGUILayout.IntField(count, GUILayout.MaxWidth(30));
-        if(add)
+        if(add && count.intValue > 0)
         {
             //Instantiate objects
             if (bulletPoolParent == null)
                 bulletPoolParent = new GameObject("BulletPoolParent");
+
+            for (int i = 0; i < count.intValue; i++)
+            {
+                GameObject go = Instantiate(bulletPrefab.objectReferenceValue as GameObject, bulletPoolParent.transform);
+                Bullet curBullet = go.GetComponent<Bullet>();
+                curBullet.originTransform = selfTransform.objectReferenceValue as Transform;
+                script.bullets.Add(curBullet);
+            }
         }
-        if(remove)
+        if(remove && count.intValue > 0)
         {
             //Remove/Destroy objects
+            for (int i = 0; i < count.intValue; i++)
+            {
+                DestroyImmediate(script.bullets[script.bullets.Count - 1].gameObject);
+                script.bullets.RemoveAt(script.bullets.Count - 1);
+            }
         }
 
         EditorGUILayout.Space();
@@ -52,9 +98,12 @@ public class CharacterAttackInspector : Editor
         {
             //Draw Error
         }
-
+       
         EditorGUILayout.EndHorizontal();
+        EditorGUILayout.PropertyField(bullets);
+        #endregion
 
+        serializedObject.ApplyModifiedProperties();
     }
 
 }
